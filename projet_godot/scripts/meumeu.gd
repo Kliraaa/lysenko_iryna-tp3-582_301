a3dc083b6ebbd601809e00f1fdb55e562909c2df
Kliraaa <2472632@cmontmorency.qc.ca>
2025-11-06 22:03:57 -0500

    menu

--- projet_godot/scripts/meumeu.gd
+++ projet_godot/scripts/meumeu.gd
@@ -1,17 +1,5 @@
 extends Control
  
- 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
- 
- 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
- 
-
-
 func _on_commencer_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/niveau_interieur_intro.tscn")
 
