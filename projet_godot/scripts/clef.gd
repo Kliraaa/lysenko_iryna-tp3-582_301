44c0e21d208cb88eb11fd15a01778bf7ede7d3b3
Kliraaa <2472632@cmontmorency.qc.ca>
2025-11-13 23:22:56 -0500

    Made transitions between doors

--- projet_godot/scripts/clef.gd
+++ projet_godot/scripts/clef.gd
@@ -3,7 +3,7 @@ extends Area2D
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $PickupSound
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	print("Vous avez pris un clef")
 	animation_player.play("pickup")
 	audio_stream_player_2d.play()
